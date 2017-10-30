9e4219ed651414be0c9e448a560bcbada8fa02d6
Gerardo Belot <gbelot2003@hotmail.com>
2017-10-30 12:50:41 -0600

    creando relacion de usuarios a Jokes

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function jokes()
+    {
+        return $this->hasMany(Joke::class, 'user_id', 'id');
+    }
 }
