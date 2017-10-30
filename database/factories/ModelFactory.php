06c95458358425d5191d76d6233aa524d48d4877
Gerardo Belot <gbelot2003@hotmail.com>
2017-10-30 12:51:01 -0600

    Agregando generador de Jokes en Model Factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,13 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Joke::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentences(3),
+        'jokes' => $faker->paragraphs(6),
+
+    ];
+});
+
+
